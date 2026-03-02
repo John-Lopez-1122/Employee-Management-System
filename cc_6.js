60a637426d0aa4e39095db6684b524fe99d0bc0b
John-Lopez-1122 <jlopez354@usf.edu>
2026-03-01 21:25:24 -0500

    added Company class

--- cc_6.js
+++ cc_6.js
@@ -18,9 +18,32 @@ class manager extends employee {
     }
 }
 
-const emp1 = new employee("Spike the dragon", "library")
-const mgr1 = new manager("Twilight Sparkle", "library", 3)
-const emp2 = new employee("Fluttershy", "Shelter")
-const mgr2 = new manager("Angel", "Shelter", 2)
-const emp3 = new employee("Rarity", "Boutique")
-const mgr3 = new manager("Rarity", "Boutique", 3)
+const emp1 = new employee("Spike the dragon", "The library")
+const mgr1 = new manager("Twilight Sparkle", "The library", 3)
+const emp2 = new employee("Fluttershy", "The shelter")
+const mgr2 = new manager("Angel", "The shelter", 2)
+const emp3 = new employee("Rarity", "The boutique")
+const mgr3 = new manager("Rarity", "The boutique", 3)
+
+class Company {
+    constructor(name) {
+        this.name = name
+        this.employee = []
+    }
+        addEmployee(employee){
+            this.employee.push(employee)
+    }
+
+        listEmployees(){
+            this.employee.forEach(emp => console.log(emp.describe()));
+    }
+}
+
+Company.addEmployee(emp1)
+Company.addEmployee(emp2)
+Company.addEmployee(emp3)
+Company.addEmployee(mgr1)
+Company.addEmployee(mgr2)
+Company.addEmployee(mgr3)
+
+Company.listEmployees()
