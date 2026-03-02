35267cada4d0f7754c950f8e950e8a11e811d0c7
John-Lopez-1122 <jlopez354@usf.edu>
2026-03-01 20:26:23 -0500

    first commit

---
+++ cc_6.js
@@ -0,0 +1,19 @@
+class employee {
+    constructor(name, department) {
+        this.name = name
+        this.department = department
+    }
+    describe(){
+        return `${this.name} works in ${this.department}`
+    }
+}
+
+class manager extends employee {
+    constructor(name, dept, teamSize) {
+        super(name, dept)
+        this.teamSize = teamSize        
+    }
+    describe(){
+        return `${this.name} manages ${this.teamSize} people in ${this.department}`
+    }
+}
